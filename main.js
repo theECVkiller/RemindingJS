11a86a1c20ca9b4f20e51965a306092bb069f4fa
Emmanuel Valdez <emmanuelvaldez2906@gmail.com>
2025-12-02 16:21:43 -0400

    New Update

---
+++ main.js
@@ -0,0 +1,11 @@
+persona = {
+    "Nombre":"Emmanuel",
+    "Edad":"19 años",
+    "Nacimiento":"02-06-2006"
+};
+
+tag = ["Nombre: ", "Edad: ", "Nacimiento: "];
+
+for (let i = 0; i < tag; i++) {
+    
+}
