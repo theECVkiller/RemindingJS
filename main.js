c5b47dd0ff094371c0235b0641aa832395716710
Emmanuel Valdez <emmanuelvaldez2906@gmail.com>
2025-12-02 17:47:50 -0400

    New Update

--- main.js
+++ main.js
@@ -11,3 +11,14 @@ for (let i = 0; i < tag.length; i++) {
     document.writeln(persona[tagSetup]);
 }
 
+do {
+    var age = prompt(`Ingrese su edad para acceder!`);
+    if (age < 18) {
+        alert("No.")
+        break;
+    }
+    for (let i = 0; i <= 10; i++){
+        alert(`Proceso #${i} (completado).`);
+    }
+    age = 0;
+} while (age >= 18);
